146c45e85f0f2a52b49892ebd1eea09be26486f6
GarciAdam <ademadem1230@gmail.com>
2021-01-11 17:18:14 +0100

    final steps

--- main.js
+++ main.js
@@ -11,119 +11,112 @@ $(document).ready(function () {
   $('.greeting').hide();
 
   $("#hbtn").on("click", function () {
-
     $(".mouse").hide();
     $(".about").hide();
     $(".home").hide();
-    // $(".rec").hide();
+    $(".rec").hide();
     $(".headd").hide();
     $(".padd").hide();
-    $(".fr").hide();
+    $('.fr').hide();
     $(".rec").show();
     $(".vbar").show();
   });
 
+  $("#key").on("click", function () {
+    $('.fr').hide();
+    $(".mouse").hide();
+    $(".about").hide();
+    $(".home").hide();
+    $(".rec").hide();
+    $(".headd").hide();
+    $(".padd").hide();
+    $(".Keyboard").show();
+    $('.greeting').hide();
+  });
 
+  $("#ms").on("click", function () {
+    $(".Keyboard").hide();
+    $('.fr').hide();
+    $(".about").hide();
+    $(".home").hide();
+    $(".rec").hide();
+    $(".headd").hide();
+    $(".padd").hide();
+    $(".mouse").show();
+    $('.greeting').hide();
+  });
 
-$("#key").on("click", function () {
-  $('.fr').hide();
-  $(".mouse").hide();
-  $(".about").hide();
-  $(".home").hide();
-  $(".rec").hide();
-  $(".headd").hide();
-  $(".padd").hide();
-  $(".Keyboard").show();
-  $(".greeting").hide();
+  $("#hd").on("click", function () {
+    $(".Keyboard").hide();
+    $('.fr').hide();
+    $(".about").hide();
+    $(".home").hide();
+    $(".rec").hide();
+     $(".mouse").hide();
+     $(".padd").hide();
+     $(".headd").show();
+     $('.greeting').hide();
+  });
 
-});
+  $("#mp").on("click", function () {
+    $(".Keyboard").hide();
+    $('.fr').hide();
+    $(".about").hide();
+    $(".home").hide();
+    $(".rec").hide();
+    $(".mouse").hide();
+    $(".headd").hide();
+    $(".padd").show();
+    $('.greeting').hide();
+  });
 
-$("#ms").on("click",function () {
-  $(".Keyboard").hide();
-  $(".fr").hide();
-  $(".about").hide();
-  $(".home").hide();
-  $(".rec").hide();
-  $(".headd").hide();
-  $(".padd").hide();
-  $(".mouse").show();
-  $(".greeting").hide();
-});
+  $('#home').on("click",function(){
+    $(".rec").hide();
+    $('.fr').hide();
+    $(".about").hide();
+    $(".vbar").hide();
+    $(".Keyboard").hide();
+    $(".mouse").hide();
+    $(".headd").hide();
+    $(".padd").hide();
+    $(".frm").hide();
+    $(".home").show();
+    $('.greeting').hide();
+  })
+  $("#abt").on("click",function(){
+    $(".rec").hide();
+    $(".about").hide();
+    $('.fr').hide();
+    $(".vbar").hide();
+    $(".Keyboard").hide();
+    $(".mouse").hide();
+    $(".headd").hide();
+    $(".padd").hide();
+    $(".frm").hide();
+    $(".home").hide()
+    $(".about").show();
+    $('.greeting').hide();
+  
 
-$("#hd").on("click",function () {
-  $(".Keyboard").hide();
-  $(".fr").hide();
-  $(".about").hide();
-  $(".home").hide();
-  $(".rec").hide();
-  $(".mouse").hide();
-  $(".padd").hide();
-  $(".headd").show();
-  $(".greeting").hide();
-});
+  })
 
+  $('input').on("click",function(){
+    $(".rec").hide();
+    $(".vbar").hide();
+    $(".Keyboard").hide();
+    $(".mouse").hide();
+    $(".headd").hide();
+    $(".padd").hide();
+    $(".frm").hide();
+    $(".about").hide();
+    $('.greeting').hide();
+    $('.fr').show();
 
-$("#mp").on("click",function () {
-  $(".Keyboard").hide();
-  $('.fr').hide();
-  $(".about").hide();
-  $(".home").hide();
-  $(".rec").hide();
-  $(".mouse").hide();
-  $(".headd").hide();
-  $(".padd").show();
-  $(".greeting").hide();
-});
+  })
 
-
-
-
-$("#home").on("click",function () {
-  $(".rec").show();
-  $(".fr").hide();
-  $(".about").hide();
-  $(".vbar").hide();
-  $(".Keyboard").hide();
-  $(".mouse").hide();
-  $(".headd").hide();
-  $(".padd").hide();
-  $(".frm").hide();
-  $(".home").show();
-  $('.greeting').hide();
-   
- });
-
-$("#abt").on("click",function () {
-  $(".rec").hide();
-  $(".about").hide();
-  $('.fr').hide();
-  $(".vbar").hide();
-  $(".Keyboard").hide();
-  $(".mouse").hide();
-  $(".headd").hide();
-  $(".padd").hide();
-  $(".frm").hide();
-  $(".home").hide();
-  $(".about").show();
-  $(".greeting").hide();
-});
-
-$('input').on("click",function(){
-	$(".rec").hide();
-	$(".vbar").hide();
-	$("Keyboard").hide();
-	$(".mouse").hide();
-	$(".headd").hide();
-	$(".padd").hide();
-	$(".frm").hide();
-	$(".about").hide();
-	$(".greeting").hide();
-	$(".fr").show();
-});
-
-
-
-});
-
+ 
 
   
+
+  
+});
