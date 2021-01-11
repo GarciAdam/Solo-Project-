f551c078688d9a448362885ab850fc33a35d0c3f
GarciAdam <ademadem1230@gmail.com>
2021-01-11 12:42:06 +0100

    $

--- main.js
+++ main.js
@@ -17,7 +17,58 @@ $(document).ready(function () {
     $(".rec").hide();
     $(".headd").hide();
     $(".padd").hide();
-    $('.fr').hide();
+    $(".fr").hide();
     $(".rec").show();
     $(".vbar").show();
   });
+
+
+
+$("#key").on("click", function () {
+  $(".mouse").hide();
+  $(".about").hide();
+  $(".home").hide();
+  $(".rec").hide();
+  $(".headd").hide();
+  $(".pad").hide();
+  $(".Keyboard").show();
+  $(".greeting").hide();
+
+});
+
+$(."#ms").on("click",function () {
+  $(".Keyboard").hide();
+  $(".fr").hide();
+  $(".about").hide();
+  $(".home").hide();
+  $(".rec").hide();
+  $(".headd").hide();
+  $(".padd").hide();
+  $(".mouse").show();
+  $(".greeting").hide();
+})
+
+$(."#hd").on("click",function () {
+  $(".Keyboard").hide();
+  $(".fr").hide();
+  $(".about").hide();
+  $(".home").hide();
+  $(".rec").hide();
+  $(".mousse").hide();
+  $(".padd").hide();
+  $(".headd").show();
+  $(".greeting").hide();
+})
+
+$("#home").on("click",function () {
+  $(".rec").hide();
+  $(".fr").hide();
+  $(".about").hide();
+  $(".vbar").hide();
+  $(".Keyboard").hide();
+  $(".mouse").hide();
+  $(".headd").hide();
+  $(".padd").hide();
+  $(".frm").hide();
+  $('.greeting'.hide();
+   $(".home").show();
