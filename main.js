8263ad70701d602caf76182aee4b229b6e99b2d1
GarciAdam <ademadem1230@gmail.com>
2021-01-11 11:48:19 +0100

    steps

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-     
+$(document).ready(function () {
+  $(".rec").hide();
+  $(".vbar").hide();
+  $(".Keyboard").hide();
+  $(".mouse").hide();
+  $(".headd").hide();
+  $(".padd").hide();
+  $(".frm").hide();
+  $(".about").hide();
+  $('.fr').hide();
+  $('.greeting').hide();
+
+  $("#hbtn").on("click", function () {
+    $(".mouse").hide();
+    $(".about").hide();
+    $(".home").hide();
+    $(".rec").hide();
+    $(".headd").hide();
+    $(".padd").hide();
+    $('.fr').hide();
+    $(".rec").show();
+    $(".vbar").show();
+  });
