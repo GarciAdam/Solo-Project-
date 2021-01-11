d045ee4cb5811cdcbe37944a2a2952b1bda7c608
GarciAdam <ademadem1230@gmail.com>
2021-01-11 23:01:14 +0100

    add

--- main.js
+++ main.js
@@ -115,7 +115,14 @@ $(document).ready(function () {
   })
 
  
-
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+  
   
 
   
